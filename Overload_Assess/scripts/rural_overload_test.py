f232df3c2bb7be5d9c4dab130c2513410378c97e
Abhisek Konar <konarabhisek@gmail.com>
2025-02-06 14:27:04 -0500

    Modified file paths and added the ability to run the script in loops to account for different charging patterns.

--- Overload_Assess/scripts/rural_overload_test.py
+++ Overload_Assess/scripts/rural_overload_test.py
@@ -12,7 +12,7 @@ from build_net import build_net_rural
 from util import get_bus_index_by_name 
 import os
 import numpy as np
-
+import glob
 from copy import deepcopy
 import ipdb 
 
@@ -28,7 +28,7 @@ def loading_assess_rural(net, nonEV_info , EV_load = None):
     
     '''
     
-    dt = pd.read_excel('./data/nonEV_norm.xlsx')
+    dt = pd.read_excel('../data/nonEV_norm.xlsx')
     
     normalized_res_profile = dt['residential']
     
@@ -95,68 +95,80 @@ if __name__=='__main__':
     '''    
 
     # 
-    rural_info = pd.read_excel('./data/rural_net_household_distribution.xlsx' ).iloc[:,[0,2]]
-    ldev_penetration_rate = pd.read_csv('./data/ldev_load_data/penetration_rate_ldev.csv') #in percentage
-    charger_info = pd.read_excel("./data/ldev_load_data/chargers_for_1000_vehicles.xlsx")
-    ev_load = pd.read_excel('./data/ldev_load_data/EV_load_profiles_daily_req.xlsx') 
+    rural_info = pd.read_excel('../data/rural_net_household_distribution.xlsx' ).iloc[:,[0,2]]
+    ldev_penetration_rate = pd.read_csv('../data/ldev_load_data/penetration_rate_ldev.csv') #in percentage
+    charger_info = pd.read_excel("../data/ldev_load_data/chargers_for_1000_vehicles.xlsx")
+
+    # adding multiple EV load pattern files
+    base_folder_ev_charging_pattern = '../data/ldev_load_data/alternate_charging_scenarios/profiles_v5'
+
+    ev_load_profile_files = glob.glob(f'{base_folder_ev_charging_pattern}/EV_load_*.xlsx') 
+    #ev_load = pd.read_excel('./data/ldev_load_data/EV_load_profiles_daily_req.xlsx') 
 
     rural_grouped = rural_info .groupby('Bus', as_index=False).sum()    
     # rural_grouped.iloc[:,1] = rural_grouped.iloc[:,1].astype('float64')
     
-    
-    grow_rate = 1.018**5
-    # fig = plt.figure(figsize=(10,15))
-    k=1
-    # for year in  [2015]:
-    data_dict = {}
-    for year in range(2025, 2055,5):
 
-        print('running the year of {}'.format(year))
-        E_daily_energy = 30*1e-3 *k   # daily energy of a household  [MWh]
-        k*= grow_rate
-          
-        net = build_net_rural ()
-        
-        charger_info_year = charger_info[charger_info['year']==year]
-        # adding Non EV load 
-        nonEV_info = deepcopy(rural_grouped)
-        nonEV_info.iloc[:,1] *= E_daily_energy 
-        nonEV_info = nonEV_info.rename(columns={'NumOfHouse':'E_per_day'})
-        #adding EV load 
-        ev_load_dict = {}
-        total_evs = 0
-        total_load_l1 = total_load_l2 = np.zeros(24)
-        for bus_info in rural_grouped.itertuples():
-            num_houses = bus_info.NumOfHouse 
-            pen_rate = ldev_penetration_rate[
-                                ldev_penetration_rate['year']==year
-                                    ]['penetration_rate'].item()*1e-2
-            evs = num_houses*2*pen_rate
-            total_evs+=evs
-            l1_chargers = charger_info_year['home_l1'].item()
-            l2_chargers = charger_info_year['home_l2'].item() 
-            l1_l2_ratio = l1_chargers/(l1_chargers+l2_chargers)
+    for ev_load_profile_file in ev_load_profile_files:
+        ev_load = pd.read_excel(ev_load_profile_file)
             
-            l1_load = (ev_load['home_AC1']*evs*l1_l2_ratio)*(1e-3)
-            l2_load = (ev_load['home_AC2']*evs*(1-l1_l2_ratio))*(1e-3)
-            ev_load_dict[f'bus_{bus_info.Bus}'] = l1_load+l2_load 
-            total_load_l1 += l1_load
-            total_load_l2 += l2_load    
+        parent_folder = ev_load_profile_file.split('/')[-2]
+        ev_load_fname = ev_load_profile_file.split('/')[-1].split('.')[0]
+        grow_rate = 1.018**5
+        # fig = plt.figure(figsize=(10,15))
+        k=grow_rate**2
+        data_dict = {}
 
-        data_dict[year] = {'num_evs': total_evs, 
-                           'pen_rate': pen_rate, 
-                           'L1_L2_ratio': l1_l2_ratio, 
-                           'L1 load' : total_load_l1, 
-                           'L2_load': total_load_l2}
-        
-        print(f"Num EVs: {total_evs} \n Penetration rate: {pen_rate} \n total load l1: {total_load_l1} \n total load l2: {total_load_l2} ")
-        output_table_tra,  output_table_line= loading_assess_rural(net, 
-                                                                   nonEV_info, 
-                                                                   ev_load_dict ) 
+        for year in range(2025, 2055,5):
+
+            print('running the year of {}'.format(year))
+            E_daily_energy = 30*1e-3 *k   # daily energy of a household  [MWh]
+            k*= grow_rate
+            
+            net = build_net_rural ()
+            
+            charger_info_year = charger_info[charger_info['year']==year]
+            # adding Non EV load 
+            nonEV_info = deepcopy(rural_grouped)
+            nonEV_info.iloc[:,1] *= E_daily_energy 
+            nonEV_info = nonEV_info.rename(columns={'NumOfHouse':'E_per_day'})
+            #adding EV load 
+            ev_load_dict = {}
+            total_evs = 0
+            total_load_l1 = total_load_l2 = np.zeros(24)
+            for bus_info in rural_grouped.itertuples():
+                num_houses = bus_info.NumOfHouse 
+                pen_rate = ldev_penetration_rate[
+                                    ldev_penetration_rate['year']==year
+                                        ]['penetration_rate'].item()*1e-2
+                evs = num_houses*2*pen_rate
+                total_evs+=evs
+                l1_chargers = charger_info_year['home_l1'].item()
+                l2_chargers = charger_info_year['home_l2'].item() 
+                l1_l2_ratio = l1_chargers/(l1_chargers+l2_chargers)
+                
+                l1_load = (ev_load['home_AC1']*evs*l1_l2_ratio)*(1e-3)
+                l2_load = (ev_load['home_AC2']*evs*(1-l1_l2_ratio))*(1e-3)
+                ev_load_dict[f'bus_{bus_info.Bus}'] = l1_load+l2_load 
+                total_load_l1 += l1_load
+                total_load_l2 += l2_load    
+
+            data_dict[year] = {'num_evs': total_evs, 
+                            'pen_rate': pen_rate, 
+                            'L1_L2_ratio': l1_l2_ratio, 
+                            'L1 load' : total_load_l1, 
+                            'L2_load': total_load_l2}
+            
+            print(f"Num EVs: {total_evs} \n Penetration rate: {pen_rate} \n total load l1: {total_load_l1} \n total load l2: {total_load_l2} ")
+            output_table_tra,  output_table_line= loading_assess_rural(net, 
+                                                                    nonEV_info, 
+                                                                    ev_load_dict ) 
 
 
-        folder = './Results/Results_rural_daily_req/'
-        os.makedirs(os.path.dirname(folder), exist_ok=True)
-        pd.DataFrame(data_dict).to_excel('{}evinfo.xlsx'.format(folder, year))
-        output_table_tra.to_excel('{}EV{}_tra.xlsx'.format(folder, year))
-        output_table_line.to_excel('{}EV{}_line.xlsx'.format(folder, year))
+            folder = f'../results/Results_{parent_folder}_{ev_load_fname}_rural/'
+            os.makedirs(os.path.dirname(folder), exist_ok=True)
+            os.makedirs(os.path.dirname(folder), exist_ok=True)
+            pd.DataFrame(ev_load_dict).to_csv(f'{folder}/{year}_ldev_load.csv')
+            pd.DataFrame(data_dict).to_excel('{}evinfo.xlsx'.format(folder, year))
+            output_table_tra.to_excel('{}EV{}_tra.xlsx'.format(folder, year))
+            output_table_line.to_excel('{}EV{}_line.xlsx'.format(folder, year))
